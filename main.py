a9e732f23ae04136042169ea3e3f3b36f0c4a904
kh2ffx <kh2ffx@gmail.com>
2025-03-17 21:45:56 -0500

    commit 3

--- main.py
+++ main.py
@@ -3,12 +3,15 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
-    
     # create game window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0 # delta_time
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     # infinite loop
     while True:
@@ -19,14 +22,17 @@ def main():
                 return
         
         # fill game window with black
-        pygame.Surface.fill(screen, (0, 0, 0))
+        screen.fill("black")
+
+        # draw player
+        player.draw(screen)
 
         # refresh window
         pygame.display.flip()
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        # tick
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
+
